3a45a95db7f6bdf6c86ff7d65f973de2265d7d80
Paulius Gudaciauskas <paulius.gudaciauskas@nortal.com>
2019-08-02 11:12:23 +0300

    Small changes

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,12 +4,18 @@ import { AuthComponent } from './containers/auth/auth.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [AuthComponent, LoginComponent, SignUpComponent],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule
   ],
   exports: [AuthComponent],
 })
